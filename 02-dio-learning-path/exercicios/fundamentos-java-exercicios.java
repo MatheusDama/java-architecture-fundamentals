f3852e84fbde154bf91c6bb2cc7f45b739ad8d58
Matheus Santos Damasceno <matheusdamasceno950@hotmail.com>
2026-06-19 20:43:46 +0000

    Update fundamentos-java-exercicios.java

--- 02-dio-learning-path/exercicios/fundamentos-java-exercicios.java
+++ 02-dio-learning-path/exercicios/fundamentos-java-exercicios.java
@@ -1,3 +1,5 @@
+//Escreva um código que receba o nome e o ano de nascimento de alguém e imprima na tela a seguinte mensagem: "Olá 'Fulano' você tem 'X' anos"
+
 import java.util.Scanner;
 
 public class Name {
